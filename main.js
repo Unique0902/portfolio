22cb63cf6e34d7e61bd7e931c127d7b8c92b1ec0
Unique0902 <89523316+Unique0902@users.noreply.github.com>
2022-03-30 14:58:53 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener('scroll', ()=>{
     }
 });
 
-// Handle Scrolling when tapping on the navbaR menu
+// Handle Scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
     const target = event.target;
@@ -28,6 +28,22 @@ const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', ()=>{
     scrollIntoView('#contact');
 });
+
+// Make home slowly fade to transparent as the window scrolls down
+const home= document.querySelector('#home');
+const homeContainer = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY>homeHeight){
+        homeContainer.style.opacity=0;
+    }
+    else {
+        homeContainer.style.opacity= 1-window.scrollY/homeHeight;
+    }
+});
+
+
+
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
